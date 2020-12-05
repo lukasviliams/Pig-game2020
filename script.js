e62f03fe9280127c26bd63a1409bcdeeccb80a84
lukasviliams <lukas.viliamsgmail.com>
2020-12-05 23:13:16 +0100

    completed

--- script.js
+++ script.js
@@ -11,7 +11,21 @@ const buttonNew = document.querySelector('.btn--new');
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonnHold = document.querySelector('.btn--hold');
 
-// Global functions
+//Current score
+let currentScore = 0;
+//Active player
+let activePlayer = 0;
+//Acumulated scores for both players
+const scores = [0, 0];
+//Game mode
+let playing = true;
+
+//hidden dice
+diceEl.classList.add('hidden');
+
+////////////////////////// Global functions
+
+//Switch player
 const switchPlayer = () => {
   //Clear score of last active player
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -25,17 +39,6 @@ const switchPlayer = () => {
 //Default score
 score0El.textContent = 0;
 score1El.textContent = 0;
-
-//hidden dice
-diceEl.classList.add('hidden');
-//Current score
-let currentScore = 0;
-//Active player
-let activePlayer = 0;
-//Acumulated scores for both players
-const scores = [0, 0];
-//Game mode
-let playing = true;
 
 /////////////// Rolling dice funcionality
 buttonRoll.addEventListener('click', () => {
@@ -83,4 +86,25 @@ buttonnHold.addEventListener('click', () => {
       switchPlayer();
     }
   }
+});
+
+//New game BTN resseting the game to initall status
+
+buttonNew.addEventListener('click', () => {
+  //reseting values
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  scores[activePlayer] = 0;
+  currentScore = 0;
+  //seting game satus to true
+  playing = true;
+  // bck remove
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  //Seting active player to initial
+  activePlayer = 0;
 });
