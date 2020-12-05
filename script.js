99441281dc1d73657f11394402e2dd5930b683eb
lukasviliams <lukas.viliamsgmail.com>
2020-12-05 15:05:46 +0100

    js update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+//Global Variables selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+//Default score
+score0El.textContent = 0;
+score1El.textContent = 0;
