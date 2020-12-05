dcbb1a7c3c74b68aa891a48dcd7da17c4dcb25a2
lukasviliams <lukas.viliamsgmail.com>
2020-12-05 16:06:24 +0100

    prepared for adding active players and more

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
-
-"use strict";
+'use strict';
 //Global Variables selecting elements
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1");
-const diceEl = document.querySelector(".dice");
-const buttonNew = document.querySelector(".btn--new");
-const buttonRoll = document.querySelector(".btn--roll");
-const buttonnHold = document.querySelector(".btn--hold");
-
-
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const buttonNew = document.querySelector('.btn--new');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonnHold = document.querySelector('.btn--hold');
 
 //Default score
 score0El.textContent = 0;
 score1El.textContent = 0;
 
 //hidden dice
-diceEl.classList.add("hidden");
+diceEl.classList.add('hidden');
+//Current score
+let currentScore = 0;
 
 // Rolling dice funcionality
-buttonRoll.addEventListener("click", () => {
+buttonRoll.addEventListener('click', () => {
   //1. generate a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
   //2. display the dice
-  diceEl.classList.remove("hidden");
-  diceEl.src = "dice--${dice}.png";
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 
-  //Check for rolled 1: if true switch to next player
+  //Check for rolled 1: if true
+  if (dice !== 1) {
+    //Add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //change later
+  } else {
+    // Clear the score of active player
+    current0El.textContent = 0;
+    //switch to next player
+  }
 });
 
+//Add score to the Gloal score of the Atiev Player afetr BTN Hold
+
+// Clear the current score of active player after btn hold
+
+//switch to the next player after btn hold
+
+// Swith to the next player after 1 is rolled
+
+//Create active plaers var
+
+//Switch colors to active players
+
+//Reset veerzthing after pressing new game
